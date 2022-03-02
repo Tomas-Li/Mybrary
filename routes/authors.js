d163ef00368e25d4cc81751642da1de7d447131b
Tomas Linan <tomas.m.l.001@gmail.com>
2022-03-01 23:22:15 -0300

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+// All authors routes
+router.get('/', async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name != '') {
+        searchOptions.name = new RegExp(req.query.name, 'i');
+    }//it's a req.query because the form used a GET method (param inside url)!
+    try {
+        const authors = await Author.find(searchOptions);
+        console.log(authors);
+        res.render("authors/index", { 
+            authors: authors,
+            searchOptions: req.query
+        });;
+    } catch {
+        res.redirect('/');
+    }
+})
+
+// New authors routes (form for creation)
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
+})
+
+// Create author route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    });
+    try{
+        const newAuthor = await author.save(); 
+        // res.redirect(`authors/${newAuthor.id}`);
+        res.redirect(`authors`)
+        // if gucchi then redirect to the new input's page
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating Author'
+        }); //If err then reload the page with the inputs and a error msg
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,35 @@
+// Checking the env situation
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+// Imports
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 
+// Mongoose connection
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connection to MongoDB Succesful'));
 
+// Routing
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
+// Initializing express
 const app = express();
 
+// Express Configuration and Uses
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/views');//This tell express that our .ejs will be there!
 app.set('layout', 'layouts/layout'); //folder layout, file layout
 app.use(expressLayouts);
 app.use(express.static('public')); //We are telling the folder where our static files will be!
+app.use(express.urlencoded( { extended: false , limit: '10mb'} ));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000); //For dev is 3000, when it's a real depolyment is an env variable
